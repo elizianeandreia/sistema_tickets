24f8f061a60b77e4466565b13c51c5d9036285af
Seu Nome <liziandreia@gmail.com>
2026-03-08 11:19:03 -0300

    Update App.jsx

--- suporte/src/App.jsx
+++ suporte/src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import './assets/index.css';
+import './App.css';
 
 export default function App() {
   const [tickets, setTickets] = useState(() => {
